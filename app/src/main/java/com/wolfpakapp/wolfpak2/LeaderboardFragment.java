ddfa41e525b829f3cef20a033dc1b072ce7d12c1
Raziq <mohideen.syed@rocketmail.com>
2015-07-15 02:39:56 -0400

    LeaderboardTabManager manages each leaderboard

--- app/src/main/java/com/wolfpakapp/wolfpak2/LeaderboardFragment.java
+++ app/src/main/java/com/wolfpakapp/wolfpak2/LeaderboardFragment.java
@@ -1,20 +1,13 @@
 package com.wolfpakapp.wolfpak2;
 
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v4.app.Fragment;
-import android.support.v4.widget.SwipeRefreshLayout;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TabHost;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
 public class LeaderboardFragment extends Fragment implements TabHost.TabContentFactory {
 
@@ -22,16 +15,13 @@ public class LeaderboardFragment extends Fragment implements TabHost.TabContentF
     final String ALL_TIME_TAG = "all_time_leaderboard";
     final String DEN_TAG = "den_board";
 
-    HashMap<String, ArrayList<LeaderboardListItem>> mLeaderboardListMap;
+    HashMap<String, LeaderboardTabManager> mTabManagerMap;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        mLeaderboardListMap = new HashMap<>();
-        mLeaderboardListMap.put(LOCAL_TAG, new ArrayList<LeaderboardListItem>());
-        mLeaderboardListMap.put(ALL_TIME_TAG, new ArrayList<LeaderboardListItem>());
-        mLeaderboardListMap.put(DEN_TAG, new ArrayList<LeaderboardListItem>());
+        mTabManagerMap = new HashMap<>();
     }
 
     @Override
@@ -52,23 +42,8 @@ public class LeaderboardFragment extends Fragment implements TabHost.TabContentF
 
     @Override
     public View createTabContent(String tag) {
-        final SwipeRefreshLayout swipeRefreshLayout = (SwipeRefreshLayout) LayoutInflater
-                .from(getActivity()).inflate(R.layout.tab_leaderboard, null);
+        LeaderboardTabManager manager = new LeaderboardTabManager(tag, this);
 
-        RecyclerView recyclerView = (RecyclerView) swipeRefreshLayout
-                .findViewById(R.id.leaderboard_recycler_view);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-
-        LeaderboardTabAdapter leaderboardTabAdapter =
-                new LeaderboardTabAdapter(mLeaderboardListMap.get(tag));
-
-        recyclerView.setAdapter(leaderboardTabAdapter);
-
-        mLeaderboardListMap.get(tag).add(new LeaderboardListItem());
-
-        leaderboardTabAdapter.notifyDataSetChanged();
-
-        return swipeRefreshLayout;
+        return manager.getTabLayout();
     }
 }
---
+++ app/src/main/java/com/wolfpakapp/wolfpak2/LeaderboardTabManager.java
@@ -0,0 +1,46 @@
+package com.wolfpakapp.wolfpak2;
+
+import android.app.Activity;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+
+import java.util.ArrayList;
+
+public class LeaderboardTabManager {
+    SwipeRefreshLayout mSwipeRefreshLayout;
+    RecyclerView mRecyclerView;
+
+    ArrayList<LeaderboardListItem> leaderboardListItems;
+
+    LeaderboardFragment mParentFragment;
+    final String tag;
+
+    public LeaderboardTabManager(String tag, LeaderboardFragment mParentFragment) {
+        this.tag = tag;
+        this.mParentFragment = mParentFragment;
+
+        mSwipeRefreshLayout = (SwipeRefreshLayout) LayoutInflater
+                .from(mParentFragment.getActivity()).inflate(R.layout.tab_leaderboard, null);
+
+        mRecyclerView = (RecyclerView) mSwipeRefreshLayout.findViewById(R.id.leaderboard_recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(mParentFragment.getActivity()));
+
+        leaderboardListItems = new ArrayList<>();
+
+        LeaderboardTabAdapter leaderboardTabAdapter =
+                new LeaderboardTabAdapter(leaderboardListItems);
+
+        mRecyclerView.setAdapter(leaderboardTabAdapter);
+
+        leaderboardListItems.add(new LeaderboardListItem());
+
+        leaderboardTabAdapter.notifyDataSetChanged();
+    }
+
+    public SwipeRefreshLayout getTabLayout() {
+        return mSwipeRefreshLayout;
+    }
+}
