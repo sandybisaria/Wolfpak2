95e9b2b01f89dda14407cdef21b52208cd702f16
Raziq <mohideen.syed@rocketmail.com>
2015-07-20 19:31:45 -0400

    Continued documentation

--- app/src/main/java/com/wolfpakapp/wolfpak2/leaderboard/LeaderboardFragment.java
+++ app/src/main/java/com/wolfpakapp/wolfpak2/leaderboard/LeaderboardFragment.java
@@ -16,6 +16,9 @@ import org.json.JSONObject;
 
 import java.util.HashMap;
 
+/**
+ * The LeaderboardFragment represents the leaderboard screen of the Wolfpak application.
+ */
 public class LeaderboardFragment extends Fragment implements TabHost.TabContentFactory {
     public static final String LOCAL_TAG = "local_leaderboard";
     public static final String ALL_TIME_TAG = "all_time_leaderboard";
@@ -31,6 +34,7 @@ public class LeaderboardFragment extends Fragment implements TabHost.TabContentF
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // Initialize the HashMaps that will be used by the fragment.
         mTabManagerMap = new HashMap<>();
         mRequestParamsMap = new HashMap<>();
         setUpRequestParams();
@@ -46,17 +50,24 @@ public class LeaderboardFragment extends Fragment implements TabHost.TabContentF
         mBaseFrameLayout = (FrameLayout) baseLayout.findViewById(R.id.leaderboard_base_frame_layout);
 
         TabHost tabHost = (TabHost) baseLayout.findViewById(android.R.id.tabhost);
+        // REQUIRED for a TabHost loaded with findViewById()
         tabHost.setup();
+
+        // Necessary for the advanced tab behaviors
         ((WolfpakTabHost) tabHost).setParentFragment(this);
 
+        // Set up the tabs
         //TODO If you click on the tab you are on, then scroll to the top of the list?
         tabHost.addTab(tabHost.newTabSpec(LOCAL_TAG).setIndicator("Local").setContent(this));
         tabHost.addTab(tabHost.newTabSpec(ALL_TIME_TAG).setIndicator("All Time").setContent(this));
         tabHost.addTab(tabHost.newTabSpec(DEN_TAG).setIndicator("Den").setContent(this));
 
+        // The leaderboard fragment is a fullscreen fragment. This is necessary so that the expanded
+        // content can take up the whole screen (dismissing the status bar in the process).
         getActivity().getWindow().getDecorView()
                 .setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
                         View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        // Add padding to the top of the layout so that it doesn't sink under the status bar.
         int statusBarPadding = 0;
         int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
         if (resourceId > 0) {
@@ -66,6 +77,12 @@ public class LeaderboardFragment extends Fragment implements TabHost.TabContentF
 
         return baseLayout;
     }
+
+    /**
+     * Create the content for each tab.
+     * @param tag The tag of the requested tab.
+     * @return The content (base view) of the tab.
+     */
     @Override
     public View createTabContent(String tag) {
         LeaderboardTabManager manager = new LeaderboardTabManager(tag, this);
@@ -78,6 +95,9 @@ public class LeaderboardFragment extends Fragment implements TabHost.TabContentF
         return mBaseFrameLayout;
     }
 
+    /**
+     * Set up the request parameters that each tab will use to retrieve posts from the server.
+     */
     private void setUpRequestParams() {
         RequestParams localParams = new RequestParams();
         RequestParams allTimeParams = new RequestParams();
@@ -98,63 +118,72 @@ public class LeaderboardFragment extends Fragment implements TabHost.TabContentF
         mRequestParamsMap.put(DEN_TAG, denParams);
     }
 
+    /**
+     * @param tag The tag of the current tab.
+     * @return The request parameters associated with the tab.
+     */
     public RequestParams getRequestParams(String tag) {
         return mRequestParamsMap.get(tag);
     }
 
+    /**
+     * Set up the relative URLs that each tab will use to retrieve posts from the server.
+     */
     private void setUpRelativeUrls() {
         mRelativeUrlsMap.put(LOCAL_TAG, "posts/local_leaderboard/");
         mRelativeUrlsMap.put(ALL_TIME_TAG, "/posts/all_time_leaderboard/");
         mRelativeUrlsMap.put(DEN_TAG, "/users/den/");
     }
 
+    /**
+     * @param tag The tag of the current tab.
+     * @return The relative URL associated with the tab.
+     */
     public String getRelativeUrl(String tag) {
         return mRelativeUrlsMap.get(tag);
     }
 
+    /**
+     * Return a LeaderboardListItem object created using the passed JSONObject. The tag determines
+     * how to parse the JSONObject and instantiate the LeaderboardListItem.
+     * @param tag The tag of the current leaderboard tab.
+     * @param jsonObject The JSONObject of the list item.
+     * @return A new LeaderboardListItem object.
+     */
     public LeaderboardListItem parseListItemJSONObject(String tag, JSONObject jsonObject) {
+        int id = jsonObject.optInt("id");
+        String handle = jsonObject.optString("handle");
+        boolean isImage = jsonObject.optBoolean("is_image");
+        String mediaUrl = jsonObject.optString("media_url");
+        int originalVoteCount = jsonObject.optInt("likes");
+        // "Default" like status is 0
+        int likeStatus = 0;
         switch (tag) {
             case LOCAL_TAG: {
-                int id = jsonObject.optInt("id");
-                String handle = jsonObject.optString("handle");
-                boolean isImage = jsonObject.optBoolean("is_image");
-                String mediaUrl = jsonObject.optString("media_url");
-                int originalVoteCount = jsonObject.optInt("likes");
-                int likeStatus = jsonObject.optInt("like_status");
-
-                return new LeaderboardListItem(id, handle, isImage, mediaUrl, originalVoteCount,
-                        LeaderboardListItem.VoteStatus.getVoteStatus(likeStatus));
+                likeStatus = jsonObject.optInt("like_status");
+                break;
             }
             case ALL_TIME_TAG: {
-                int id = jsonObject.optInt("id");
-                String handle = jsonObject.optString("handle");
-                boolean isImage = jsonObject.optBoolean("is_image");
-                String mediaUrl = jsonObject.optString("media_url");
-                int originalVoteCount = jsonObject.optInt("likes");
                 //TODO Get like status using API
-                int likeStatus = 0;
-
-                return new LeaderboardListItem(id, handle, isImage, mediaUrl, originalVoteCount,
-                        LeaderboardListItem.VoteStatus.getVoteStatus(likeStatus));
+                likeStatus = 0;
+                break;
             }
             case DEN_TAG: {
-                int id = jsonObject.optInt("id");
-                String handle = jsonObject.optString("handle");
-                boolean isImage = jsonObject.optBoolean("is_image");
-                String mediaUrl = jsonObject.optString("media_url");
-                int originalVoteCount = jsonObject.optInt("likes");
+                // Since the user can't like his own post, the like status takes on a different
+                // meaning...
                 //TODO Color determined by vote count?
-                int likeStatus = 0;
-
-                return new LeaderboardListItem(id, handle, isImage, mediaUrl, originalVoteCount,
-                        LeaderboardListItem.VoteStatus.getVoteStatus(likeStatus));
-            }
-            default: {
-                return null;
+                likeStatus = 0;
+                break;
             }
         }
+        return new LeaderboardListItem(id, handle, isImage, mediaUrl, originalVoteCount,
+                LeaderboardListItem.VoteStatus.getVoteStatus(likeStatus));
     }
 
+    /**
+     * @param tag The tag of the current tab.
+     * @return The tab manager associated with the tab.
+     */
     public LeaderboardTabManager getTabManager(String tag) {
         return mTabManagerMap.get(tag);
     }
--- app/src/main/java/com/wolfpakapp/wolfpak2/leaderboard/LeaderboardListItem.java
+++ app/src/main/java/com/wolfpakapp/wolfpak2/leaderboard/LeaderboardListItem.java
@@ -6,6 +6,10 @@ import android.content.res.Resources;
 
 import com.wolfpakapp.wolfpak2.R;
 
+/**
+ * The LeaderboardListItem represents a single list item on a leaderboard. Every object stores info
+ * like the ID, handle, URL of the image/video, and vote count.
+ */
 public class LeaderboardListItem {
     private int id;
     private String handle;
@@ -15,13 +19,16 @@ public class LeaderboardListItem {
     private int updatedVoteCount;
     private VoteStatus voteStatus;
 
-    public LeaderboardListItem(int id, String handle, boolean isImage, String mediaUrl, int updatedVoteCount, VoteStatus voteStatus) {
+    public LeaderboardListItem(int id, String handle, boolean isImage, String mediaUrl,
+                               int updatedVoteCount, VoteStatus voteStatus) {
         this.id = id;
         this.handle = handle;
         this.isImage = isImage;
         this.mediaUrl = mediaUrl;
         this.voteStatus = voteStatus;
         this.updatedVoteCount = updatedVoteCount;
+        // Depending on the user's vote status, the original vote count may be different than what
+        // the displayed vote count is.
         switch (voteStatus) {
             case NOT_VOTED: {
                 originalVoteCount = updatedVoteCount;
@@ -29,6 +36,7 @@ public class LeaderboardListItem {
             }
             case UPVOTED:
             case DOWNVOTED: {
+                // This "negates" the user's up- or down-vote.
                 originalVoteCount = updatedVoteCount - voteStatus.change;
                 break;
             }
@@ -55,6 +63,10 @@ public class LeaderboardListItem {
         return updatedVoteCount;
     }
 
+    /**
+     * The VoteStatus enumeration represents the three states that a post can be in. Each status has
+     * an associated color which is used to change the view count backgrounds.
+     */
     public enum VoteStatus {
         NOT_VOTED(0), UPVOTED(1), DOWNVOTED(-1);
         public final int change;
@@ -63,6 +75,12 @@ public class LeaderboardListItem {
             this.change = change;
         }
 
+        /**
+         * Since the server uses integers to represent vote statuses, this method will return the
+         * corresponding VoteStatus enum.
+         * @param change The integral like status (from the server response).
+         * @return The corresponding VoteStatus.
+         */
         static VoteStatus getVoteStatus(int change) {
             switch (change) {
                 case -1:
@@ -75,6 +93,10 @@ public class LeaderboardListItem {
             }
         }
 
+        /**
+         * @param context The Context which will be used to retrieve the color.
+         * @return The color integer corresponding to the VoteStatus enum.
+         */
         int getStatusColor(Context context) {
             Resources resources = context.getResources();
             switch (this) {
@@ -96,6 +118,10 @@ public class LeaderboardListItem {
         return voteStatus;
     }
 
+    /**
+     * Set the item's vote status, and update the vote count.
+     * @param voteStatus The new VoteStatus.
+     */
     public void setVoteStatus(VoteStatus voteStatus) {
         updatedVoteCount = originalVoteCount + voteStatus.change;
         this.voteStatus = voteStatus;
