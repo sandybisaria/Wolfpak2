d6744aeb6cb8be0e6ffcfb55c0ffdb7413a720f0
Raziq <mohideen.syed@rocketmail.com>
2015-08-25 15:25:05 -0400

    Refinements to main feed animations

--- app/src/main/java/com/wolfpakapp/wolfpak2/mainfeed/MediaView.java
+++ app/src/main/java/com/wolfpakapp/wolfpak2/mainfeed/MediaView.java
@@ -1,7 +1,12 @@
 package com.wolfpakapp.wolfpak2.mainfeed;
 
+import android.animation.Animator;
+import android.animation.ArgbEvaluator;
+import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
@@ -22,7 +27,6 @@ public class MediaView extends RelativeLayout {
 
     private ImageView mediaImageView;
     private VideoView mediaVideoView;
-//    public ImageView mediaVideoViewThumbnail;
     private View likeStatusOverlayView;
 
     /** Constructors **/
@@ -49,33 +53,63 @@ public class MediaView extends RelativeLayout {
 
         mediaImageView = (ImageView)findViewById(R.id.mediaImageView);
         mediaVideoView = (VideoView)findViewById(R.id.mediaVideoView);
-//        this.mediaVideoViewThumbnail = (ImageView) findViewById(R.id.mediaVideoViewThumbnail);
 
         likeStatusOverlayView = findViewById(R.id.likeStatusOverlayView);
+        setTint(Post.VoteStatus.NOT_VOTED);
     }
 
+    boolean hasStartedAnimating = false;
     /**
-     * Handles Changing Opaque View on Top of ImageView/VideoView Based on LikeStatus
-     * - Upvote: Green
-     * - Downvote: Red
-     * - Neutral: Transparent
+     * Set the tint of the view based on the vote status.
      *
      * @param voteStatus Status of the view
      */
-    public void setLikeStatus(Post.VoteStatus voteStatus) {
+    public void setTint(Post.VoteStatus voteStatus) {
+        int colorTo;
         switch (voteStatus) {
             case UPVOTED:
-                likeStatusOverlayView.setBackgroundColor(Color.argb(100, 0, 255, 0));
+                colorTo = Color.GREEN;
                 break;
-
             case DOWNVOTED:
-                likeStatusOverlayView.setBackgroundColor(Color.argb(100, 255, 0, 0));
+                colorTo = Color.RED;
                 break;
-
             case NOT_VOTED:
             default:
-                likeStatusOverlayView.setBackgroundColor(Color.argb(0,0,0,0));
-                break;
+                colorTo = Color.TRANSPARENT;
+        }
+        Drawable background = likeStatusOverlayView.getBackground();
+        if (background instanceof ColorDrawable) {
+            if (!hasStartedAnimating) {
+                hasStartedAnimating = true;
+                int colorFrom = ((ColorDrawable) background).getColor();
+                ObjectAnimator animator = ObjectAnimator.ofObject(likeStatusOverlayView,
+                        "backgroundColor", new ArgbEvaluator(),colorFrom, colorTo)
+                        .setDuration(350);
+                animator.addListener(new Animator.AnimatorListener() {
+                    @Override
+                    public void onAnimationStart(Animator animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        hasStartedAnimating = false;
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animation) {
+                        onAnimationEnd(animation);
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animator animation) {
+
+                    }
+                });
+                animator.start();
+            }
+        } else {
+            likeStatusOverlayView.setBackgroundColor(colorTo);
         }
     }
 
@@ -93,8 +127,6 @@ public class MediaView extends RelativeLayout {
             mediaVideoView.setVisibility(View.VISIBLE);
             mediaVideoView.setVideoPath(post.getMediaUrl());
             mediaVideoView.requestFocus();
-//            mediaVideoViewThumbnail.setVisibility(View.VISIBLE);
-//            Picasso.with(mediaVideoViewThumbnail.getContext()).load(thumbnail).into(mediaVideoViewThumbnail);
         }
     }
 
